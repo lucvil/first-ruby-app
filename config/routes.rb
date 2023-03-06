5d82b584ae139551b64c1b9dae651d7c70b826c4
lucvil <yoshimura037@g.ecc.u-tokyo.ac.jp>
2023-03-06 19:50:34 +0900

    complete to make app

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # 下をコメントあうと
+  # get"/books",to:"books#index"
+  # post"/books", to: "books#create"
+  # get"/books/new",to:"books#new",as: "new_book"
+  # get"/books/:id/edit", to:"books#edit", as: "edit_book"
+  # get"/books/:id",to:"books#show",as:"book"
+  # patch "/books/:id", to:"books#update"
+  # delete "/books/:id", to:"books#destroy"
+
+  resources :books
 end
